3305b4a551e56078e95570d26db00c7d03da8720
Emma Jouffroy <ejouffroy@ensc.fr>
2018-11-24 17:49:26 +0100

    scrollbar questions

--- Exercice1/FormQuestion.Designer.cs
+++ Exercice1/FormQuestion.Designer.cs
@@ -75,6 +75,7 @@
             this.clbReponses.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.clbReponses.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.clbReponses.FormattingEnabled = true;
+            this.clbReponses.HorizontalScrollbar = true;
             this.clbReponses.Location = new System.Drawing.Point(33, 512);
             this.clbReponses.Margin = new System.Windows.Forms.Padding(10, 3, 3, 3);
             this.clbReponses.Name = "clbReponses";
